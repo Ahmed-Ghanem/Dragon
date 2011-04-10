30ad742554f28afc7d50b270fd28e79489d85fa8
Ahmed Ghanem <x_rootex@yahoo.com>
2011-04-10 19:00:48 +0200

    First Commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Main 
+ * @author Ahmed Ghanem
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
